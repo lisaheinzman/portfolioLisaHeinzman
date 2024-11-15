6b3259d1f19a56c7ca14eac0f2387e8e64533820
lisaheinzman <lisaheinzman@knights.ucf.edu>
2024-11-15 10:30:36 -0500

    animation alternates through elements of array

--- headerAnimation.js
+++ headerAnimation.js
@@ -1,16 +1,17 @@
-const text = "designer";
+const text = ["designer", "developer", "digital artist"];
 const displayElement = document.getElementById("animated-header");
-const typeSpeed = 1000;
-const deleteSpeed = 1000;
+const typeSpeed = 500;
+const deleteSpeed = 250;
 
 let index = 0;
+let wordIndex = 0;
 let isDeleting = false;
 let timeoutId;
 
 function animation() {
-  if (!isDeleting && index <= text.length) {
+  if (!isDeleting && index <= text[wordIndex].length) {
     // Display the current substring of `text`
-    displayElement.innerText = text.slice(0, index) + "|";
+    displayElement.innerText = text[wordIndex].slice(0, index) + "|";
     index++;
 
     // Continue tyrping after a delay
@@ -18,22 +19,28 @@ function animation() {
     timeoutId = setTimeout(animation, typeSpeed); // Continue typing after a delay
 
     // if typing is complete, set isDeleting to true and timeout
-    if (index > text.length) {
+    if (index > text[wordIndex].length) {
       isDeleting = true;
       setTimeout(animation, 1000);
     }
     // if isDeleting is true and not complete, continue deleting
   } else if (isDeleting && index > 0) {
-    displayElement.innerText = text.slice(0, index) + "|";
+    displayElement.innerText = text[wordIndex].slice(0, index) + "|";
     index--;
 
     // Continue deleting after a delay
     clearTimeout(timeoutId);
     timeoutId = setTimeout(animation, deleteSpeed);
 
+    // if deleting is complete, set to false and go to the next word
     if (index === 0) {
       isDeleting = false;
       setTimeout(animation, 3000);
+      if (wordIndex < text.length - 1) {
+        wordIndex++;
+      } else if (wordIndex === text.length - 1) {
+        wordIndex = 0;
+      }
     }
   }
 }
