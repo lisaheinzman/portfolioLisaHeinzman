151c801b24873bdbd0c8cbc4e1f6cfe790e3dba5
lisaheinzman <lisaheinzman@knights.ucf.edu>
2024-11-28 16:26:40 -0500

    added the html from all javascript files to index.html and deleted them

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-import { About } from "./about.js";
-import { Skills } from "./skills.js";
-import { Resume } from "./resume.js";
-import { Projects } from "./projects.js";
 import { scrollToTop } from "./scroll.js";
 
 // Add event listener to the button
@@ -14,25 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
     button.addEventListener("click", () => {
       const targetId = button.getAttribute("data-target"); // Get the target container ID
       scrollToTop(targetId); // Scroll to the target container
-    });
+    }); //
   });
 });
-
-// Helper function to render components
-function renderComponent(containerId, component) {
-  const container = document.getElementById(containerId);
-  if (container) {
-    container.appendChild(component());
-  } else {
-    console.warn(`Container with ID "${containerId}" not found.`);
-  }
-}
-
-// Render components
-renderComponent("about-container", About);
-renderComponent("skills-container", Skills);
-renderComponent("projects-container", Projects);
-renderComponent("resume-container", Resume);
 
 // Map class names to their corresponding animation classes
 const classToAnimation = {
